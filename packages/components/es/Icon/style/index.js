57e3cb82740f3dcf2d886b1383bbace0ba89a46b
potati22 <845291072@qq.com>
2024-04-09 03:21:16 +0000

    删除文件 packages/components/es

--- packages/components/es/Icon/style/index.js
+++
@@ -1,2 +0,0 @@
-import '@potati/theme-chalk/src/base.scss';
-import '@potati/theme-chalk/src/component/icon.scss';
--- packages/components/es/Radio/index.js
+++
@@ -1,8 +0,0 @@
-import { withInstall as o } from "../utils/install.js";
-import t from "./src/radio.vue.js";
-import i from "./src/radio-group.vue.js";
-const f = o(t), n = o(i);
-export {
-  f as PotRadio,
-  n as PotRadioGroup
-};
